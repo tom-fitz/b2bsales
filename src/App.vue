99febc743799d273d2c6b9b39883a29d535350c7
Thomas Fitzgerald <thomas.fitzgerald@Foundant.com>
2020-05-16 14:26:47 -0600

    second push

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      right
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="#993333"
+      dark
+    >
+      <v-spacer></v-spacer>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-content>
+      <!-- <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+            <v-tooltip left>
+              <template v-slot:activator="{}">
+                
+              </template>
+
+              <span>Source</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </v-container> -->
+      <AboutSection/>
+    </v-content>
+
+    <!-- <v-footer
+      color="#993333"
+      app
+    >
+      <v-spacer></v-spacer>
+
+      <span class="white--text">&copy; 2019</span>
+    </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+import AboutSection from './components/about-section'
+
+export default {
+  name: 'App',
+
+  components: {
+    AboutSection,
+  },
+
+  data: () => ({
+    drawer: false,
+  }),
+  props: {
+    source: String
+  }
+};
+</script>
---
+++ src/components/about-section.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-parallax
+    dark
+    src="https://i.imgur.com/lad8NMq.png"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        <h1 class="display-1 font-weight-thin mb-4">Vuetify.js</h1>
+        <h4 class="subheading">Build your application today!</h4>
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+<script>
+export default {
+    name: "about-section",
+    data: () => ({
+        //
+    })
+}
+</script>
+<style>
+
+</style>
